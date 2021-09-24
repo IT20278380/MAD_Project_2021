de4e3f0cb7c09284a848e7e1ffd248a01815a03e
IT20278380 <IT20278380@my.sliit.lk>
2021-09-24 19:01:57 +0530

    Customer, Home page created

--- app/src/main/java/com/example/mad_project_2021/Cus_Home.java
+++ app/src/main/java/com/example/mad_project_2021/Cus_Home.java
@@ -1,12 +1,37 @@
 package com.example.mad_project_2021;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.mad_project_2021.tableModules.Computer;
+import com.example.mad_project_2021.tableModules.Computer_list;
+import com.example.mad_project_2021.tableModules.FragmentAdapter;
+import com.example.mad_project_2021.tableModules.Home_Computer;
+import com.example.mad_project_2021.tableModules.Home_Laptop;
+import com.example.mad_project_2021.tableModules.Home_com_ass;
+import com.example.mad_project_2021.tableModules.Home_com_ass_list;
+import com.example.mad_project_2021.tableModules.Home_com_list;
+import com.example.mad_project_2021.tableModules.Home_lap_ass;
+import com.example.mad_project_2021.tableModules.Home_lap_list;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.annotations.Nullable;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -49,6 +74,7 @@ public class Cus_Home extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -60,5 +86,149 @@ public class Cus_Home extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_cus__home, container, false);
+    }
+
+    RecyclerView recyclerView;
+    DatabaseReference database;
+    Home_com_list home_com_list;
+    ArrayList<Home_Computer> lst;
+
+    RecyclerView recyclerViewLep;
+    DatabaseReference databaselep;
+    Home_lap_list home_lap_list;
+    ArrayList<Home_Laptop> lstLep;
+
+    RecyclerView recyclerViewcomAss;
+    DatabaseReference databasecomAss;
+    Home_com_ass_list home_com_ass_list;
+    ArrayList<Home_com_ass> hoalist;
+
+    RecyclerView recyclerViewlapAss;
+    DatabaseReference databaselapAss;
+    Home_com_ass_list home_lap_ass_list;
+    ArrayList<Home_lap_ass> lapAss0;
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //computer
+        recyclerView = view.findViewById(R.id.home_list100);
+        database = FirebaseDatabase.getInstance().getReference("Brand_New_Computers");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+
+        lst = new ArrayList<>();
+        home_com_list = new Home_com_list(getContext(),lst);
+        recyclerView.setAdapter(home_com_list);
+
+       database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+
+                    Home_Computer home_computer = dataSnapshot.getValue(Home_Computer.class);
+                    lst.add(home_computer);
+
+
+                }
+                home_com_list.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+       ///leptop
+        recyclerViewLep = view.findViewById(R.id.home_lep100);
+        databaselep = FirebaseDatabase.getInstance().getReference("Brand_New_Laptops");
+        recyclerViewLep.setHasFixedSize(true);
+        recyclerViewLep.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+
+        lstLep = new ArrayList<>();
+        home_lap_list = new Home_lap_list(getContext(),lstLep);
+        recyclerViewLep.setAdapter(home_lap_list);
+
+        databaselep.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+
+                    Home_Laptop home_laptop = dataSnapshot.getValue(Home_Laptop.class);
+                    lstLep.add(home_laptop);
+
+
+                }
+                home_lap_list.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        //com acc
+        recyclerViewcomAss = view.findViewById(R.id.home_ass00);
+        databasecomAss = FirebaseDatabase.getInstance().getReference("Computers Accessories");
+        recyclerViewcomAss.setHasFixedSize(true);
+        recyclerViewcomAss.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+
+        hoalist = new ArrayList<>();
+        home_com_ass_list = new Home_com_ass_list(getContext(),hoalist);
+        recyclerViewcomAss.setAdapter(home_com_ass_list);
+
+        databasecomAss.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+
+                    Home_com_ass home_com_ass = dataSnapshot.getValue(Home_com_ass.class);
+                    hoalist.add(home_com_ass);
+
+
+                }
+                home_com_ass_list.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        //lep acc
+        recyclerViewlapAss = view.findViewById(R.id.home_ass11);
+        databaselapAss = FirebaseDatabase.getInstance().getReference("Laptop Accessories");
+        recyclerViewlapAss.setHasFixedSize(true);
+        recyclerViewlapAss.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+
+        lapAss0 = new ArrayList<>();
+        home_lap_ass_list = new Home_com_ass_list(getContext(),hoalist);
+        recyclerViewlapAss.setAdapter(home_lap_ass_list);
+
+        databaselapAss.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+
+                    Home_lap_ass home_lap_ass = dataSnapshot.getValue(Home_lap_ass.class);
+                    lapAss0.add(home_lap_ass);
+
+
+                }
+                home_lap_ass_list.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/mad_project_2021/Customer_Area.java
+++ app/src/main/java/com/example/mad_project_2021/Customer_Area.java
@@ -1,14 +1,30 @@
 package com.example.mad_project_2021;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.mad_project_2021.tableModules.Computer;
+import com.example.mad_project_2021.tableModules.Computer_list;
 import com.example.mad_project_2021.tableModules.FragmentAdapter;
+import com.example.mad_project_2021.tableModules.Home_Computer;
+import com.example.mad_project_2021.tableModules.Home_com_list;
+import com.example.mad_project_2021.tableModules.Lep_Acc;
+import com.example.mad_project_2021.tableModules.Lep_Acc_list;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class Customer_Area extends AppCompatActivity {
 
@@ -21,6 +37,7 @@ public class Customer_Area extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_area);
 
+
         tabLayout = findViewById(R.id.tab_layout);
         pager2 = findViewById(R.id.view_pager2);
 
@@ -31,6 +48,8 @@ public class Customer_Area extends AppCompatActivity {
         tabLayout.addTab(tabLayout.newTab().setText("Home"));
         tabLayout.addTab(tabLayout.newTab().setText("Contact"));
         tabLayout.addTab(tabLayout.newTab().setText("Profile"));
+
+
 
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
